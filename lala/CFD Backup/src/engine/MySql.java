c3815bce9c6e307ee4ad08ad5820adc129c86d1c
nonika <magyarosinonika@gmail.com>
2013-09-22 17:33:12 +0300

    meghatarozo es meghatarozott kombinaciok kiiratasa

--- lala/CFD Backup/src/engine/MySql.java
+++ lala/CFD Backup/src/engine/MySql.java
@@ -4,6 +4,7 @@
  */
 package engine;
 
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
 import java.util.AbstractList;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -22,7 +24,8 @@ import java.util.List;
 public class MySql implements Dal {
     
     ArrayList<String> primary_keys_array = new ArrayList<String>();
-    ArrayList<String> columns_array = new ArrayList<String>();
+    //ArrayList<String> columns_array = new ArrayList<String>();
+    Hashtable columns_array = new Hashtable();
 
     @Override
     public boolean isFd(String table, AbstractList<String> determinantColumns, AbstractList<String> dependentColumns) {
@@ -79,15 +82,17 @@ public class MySql implements Dal {
                     //System.out.println("Primary key column: " + result3.getString("Column_name"));
                     primary_keys_array.add(primary_keys.getString("Column_name"));
                 }
-
+                int key=1;
                 while (columns.next()) {
                     if (!(primary_keys_array.contains(columns.getString(1)))) {
                         //System.out.println("Column name: " + columns.getString(1));
-                        columns_array.add(columns.getString(1));
+                        //columns_array.add(columns.getString(1));
+                        columns_array.put(key, columns.getString(1));
+                        ++key;
                     }
                 }
                 System.out.print("Columns: ");
-                for (int i = 0; i < columns_array.size(); ++i) {
+                for (int i = 1; i <= columns_array.size(); ++i) {
                     System.out.print(columns_array.get(i) + " ");
                 }
                 System.out.println();
@@ -95,14 +100,14 @@ public class MySql implements Dal {
                 //System.out.println(columns_array.size());
                 //for (int i = 1; i <= columns_array.size(); ++i) {
                    for (int i = 1; i <= (columns_array.size() - 1); ++i) {
-                       System.out.println("megtarozo");
-                        combinations(columns_array.size(), i);
-//                        for (int j = 1; j <= columns_array.size(); ++j) {
-//                            for (int k = 1; k <= columns_array.size(); ++k) {
-//                                System.out.println("megtarozott");
-//                                combinations(columns_array.size(), j, k);
-//                            }
-//                        }
+                       //System.out.println("megtarozo");
+                        combinations(columns_array, i);
+                        for (int j = 1; j <= columns_array.size(); ++j) {
+                            for (int k = 1; k <= columns_array.size(); ++k) {
+                                //System.out.println("megtarozott");
+                                    //combinations(columns_array.size(), j, k);
+                            }
+                        }
                     }
                 //}
 
@@ -138,28 +143,28 @@ public class MySql implements Dal {
         System.out.println("delete");
     }
 
-    public void combinations(final int n, final int r) {
+    public void combinations(final Hashtable columns, final int r) {
         int[] res = new int[r];
-        //ArrayList<Integer> res = new ArrayList<Integer>();
         for (int i = 0; i < res.length; i++) {
             res[i]=i+1;
         }
         boolean done = false;
         while (!done) {
-            //System.out.println(res);
-            System.out.println(Arrays.toString(res));
-//            for(int i=0;i<columns_array.size()-1;++i){
-//                combinations(columns_array.size(), i, res);
-//            }
-//            combinations(columns_array.size(), r, null);
-//            Integer[] temp_res = new Integer[r];
-//            temp_res=res.toArray(temp_res);
-            done = getNext(res, n, r);
+            System.out.println("Meghatarozo:"+Arrays.toString(res));
+            for(int j=0;j<res.length;++j){
+                System.out.print(columns.get(res[j])+" ");
+            }
+            System.out.println();
+            for(int i=1;i<=columns_array.size()-1;++i){
+                combinations(columns_array, i, res);
+            }
+            done = getNext(res, columns.size(), r);
+            
+            
         }
     }
     
-    public void combinations(final int n, final int r, final ArrayList<Integer> melyikne) {
-        //System.out.println("From tabel: " + tableName);
+    public void combinations(final Hashtable columns, final int r, final int[] melyikne) {
         int[] res = new int[r];
         for (int i = 0; i < res.length; i++) {
             res[i] = i + 1;
@@ -168,38 +173,29 @@ public class MySql implements Dal {
         boolean jo = true;
         while (!done) {
             for (int k = 0; k < res.length; ++k) {
-                
-                    if (melyikne.contains(res[k])) {
+                for(int j=0;j<melyikne.length;++j){
+                    if(res[k]==melyikne[j]){
                         jo = false;
                         break;
                     }
-                
-
+                }
             }
             if (jo) {
-                System.out.println(Arrays.toString(res));
-//                System.out.println("Meghatarozo: "+columnsName.get(melyikne-1));
-//                System.out.print("Meghatarozott: " );
-//                for(int i=0;i<res.length;++i){
-//                    
-//                    System.out.print(columnsName.get(res[i]-1) + " ");
-//                }
-//                System.out.println();
-
-                //isFd(tableName, columnsName.get(melyikne-1), columnsName);
-
+                System.out.println("Meghatarozott:"+Arrays.toString(res));
+                for(int j=0;j<res.length;++j){
+                System.out.print(columns.get(res[j])+" ");
             }
-            done = getNext(res, n, r);
+            System.out.println();
+            }
+            done = getNext(res, columns.size(), r);
             jo = true;
         }
     }
 
-/////////
     public boolean getNext(final int[] num, final int n, final int r) {
         int target = r - 1;
         num[target]++;
         if (num[target] > ((n - (r - target)) + 1)) {
-            // Carry the One
             while (num[target] > ((n - (r - target)))) {
                 target--;
                 if (target < 0) {
